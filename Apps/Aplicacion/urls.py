5e2b657a83b9e7c20d95ebe2de8fc3d21dcf0f5a
Salchipapa822 <jhoan.yepez2038@gmail.com>
2025-02-18 13:27:00 -0400

    se han cambiado el punto de origen de las urls, se han movido a apps

--- Apps/Aplicacion/urls.py
+++ Apps/Aplicacion/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from Apps.Aplicacion.views import login_view, ticket_list, ticket_form
 
 
 urlpatterns = [
-    path('', include ('Apps.Aplicacion.urls')),
-    path('admin/', admin.site.urls),
+    path('login_view/', login_view),
+    path('tickets/', ticket_list, name='ticket_list'),
+    path('crear/', ticket_form, name='ticket_form'),
 ]
