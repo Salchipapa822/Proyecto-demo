3d7ae2a1451e309ecb103e1b930cc28dcb1f0ecd
jeancarlosjoseparedes03@gmail.com <jeancarlosjoseparedes03@gmail.com>
2025-02-20 10:15:29 -0400

    se hizo la creacion de las vistas de los detalles de tickets

--- Apps/Aplicacion/urls.py
+++ Apps/Aplicacion/urls.py
@@ -17,10 +17,14 @@ Including another URLconf
 from django.urls import path
 from Apps.Aplicacion.views import login_view, ticket_list, TicketCreateView
 from django.views.generic import TemplateView
+from .views import ticket_detalle
+from . import views
+
 
 urlpatterns = [
     path('login_view/', login_view, name='login_view'),  # Agregar nombre para la vista de login
     path('tickets/', ticket_list, name='ticket_list'),
     path('crear/', TicketCreateView.as_view(), name='ticket_create'),  # Llamar a as_view()
     path('tickets/exito/', TemplateView.as_view(template_name='ticket_success.html'), name='ticket_success'), 
+    path('ticket/<int:ticket_id>/', views.ticket_detalle, name='ticket_detalle'),  # URL para los detalles
 ]
--- Apps/Aplicacion/views.py
+++ Apps/Aplicacion/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import authenticate, login
 from .forms import TicketForm
 from .models import Ticket
 from django.views import View
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
 
@@ -20,14 +21,13 @@ def login_view(request):
             return render(request, 'login.html', {'error': 'Credenciales inválidas.'})
     return render(request, 'login.html')
 
-
-
-
+def ticket_detalle(request, ticket_id):
+    ticket = get_object_or_404(Ticket, pk=ticket_id)  # Obtén el ticket o muestra un 404 si no existe
+    return render(request, 'ticket_detalle.html', {'ticket': ticket})
 
 def ticket_list(request):
     tickets = Ticket.objects.all()  # Obtiene todos los tickets
     return render(request, 'ticket_list.html', {'tickets': tickets})
-
 
 class TicketCreateView (View):
     def get(self, request):
