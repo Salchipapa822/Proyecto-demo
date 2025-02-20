5b695b19838314107f541291dcebb7a2ec89fafa
Salchipapa822 <jhoan.yepez2038@gmail.com>
2025-02-20 12:12:07 -0400

    ahora se resalta con color rojo los no resueltos y verde los resueltos y se pueden añadir todo desde admin

--- Apps/Aplicacion/admin.py
+++ Apps/Aplicacion/admin.py
@@ -1,11 +1,40 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from . import models
-# Register your models here.
+from .models import Usuario, Administrador, Direccion, Etiqueta, Personal, Ticket, Comentario
 
+# Registro del modelo Usuario
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name')
+    search_fields = ('first_name', 'last_name', 'username')
 
-@admin.register(models.Usuario)
-class UsuarioAdmin(UserAdmin):
-    list_display = ('first_name', 'last_name', 'username')
+# Registro del modelo Direccion
+@admin.register(Direccion)
+class DireccionAdmin(admin.ModelAdmin):
+    list_display = ('nombre',)
+    search_fields = ('nombre',)
 
+# Registro del modelo Etiqueta
+@admin.register(Etiqueta)
+class EtiquetaAdmin(admin.ModelAdmin):
+    list_display = ('nombre',)
+    search_fields = ('nombre',)
 
+# Registro del modelo Personal
+@admin.register(Personal)
+class PersonalAdmin(admin.ModelAdmin):
+    list_display = ('cedula', 'nombre', 'apellido')
+    search_fields = ('nombre', 'apellido', 'cedula')
+
+# Registro del modelo Ticket
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'fecha_creacion', 'presentado_por', 'resuelto_por', 'fecha_cierre')
+    search_fields = ('titulo',)
+    list_filter = ('fecha_creacion', 'presentado_por', 'resuelto_por')
+
+# Registro del modelo Comentario
+@admin.register(Comentario)
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display = ('fecha_creacion', 'autor', 'ticket')
+    search_fields = ('contenido',)
+    list_filter = ('fecha_creacion', 'autor')
